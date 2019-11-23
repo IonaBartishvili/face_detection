e3bd42a66e105c9f7ca31fe447a90f182d09bf9b
SoulOfBelial <iona.bartishvili@gmail.com>
2019-11-23 23:05:47 +0400

    Update OpenCV.py

--- OpenCV.py
+++ OpenCV.py
@@ -1,31 +1,23 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Load the classifiers downloaded
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # Read the image and convert to grayscale format
-img = cv2.imread('people.jpg')
-img = cv2.resize(img, (500,400))
+img = cv2.imread('people3.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Calculate coordinates
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 for (x,y,w,h) in faces:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img [y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    # draw bounding boxes around detected features
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    #         starting point     End Point    RGB     Thickness
+    #                    |          |         |       |
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 3)
+    #                    text       position          FONT             Scale   RGB       Thickness
+    cv2.putText(img, 'Human face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
 
-# Plot the image
 
+img = cv2.resize(img, (438, 584))
+# Show the image
 cv2.imshow('image', img)
 cv2.waitKey(0)
-
-# Write image
-cv2.imwrite('people.jpg', img)
